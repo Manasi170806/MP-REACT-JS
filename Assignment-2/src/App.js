623f59cd4980da6bbd5de0174a41241f804c23c2
Manasi170806 <manasipatel17082006@gmail.com>
2025-07-08 12:29:43 +0530

    add-ass-02

---
+++ Assignment-2/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import MobileOS from "./components/MobileOS";
+
+function App() {
+  return (
+    <div className="App">
+      {/* add MObileOs component here */}
+      <MobileOS />
+    </div>
+  )
+}
+
+export default App;
---
+++ Assignment-2/src/components/MobileOS.jsx
@@ -0,0 +1,23 @@
+function MobileOS() {
+  return (
+    <div className="mobile">
+      <h1 className="title">Mobile Operating System</h1>
+      <ul className="sub-title">
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>iPhone</li>
+        <li>Windows Phone</li>
+      </ul>
+
+      <h1 className="heading">Mobile Manufacturers</h1>
+      <ul className="sub-heading">
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Apple</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MobileOS;
