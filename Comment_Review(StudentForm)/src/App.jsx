ae45cf6c0d0d769c3ef74b0419f0dd4c3ea6a0dc
Manasi170806 <manasipatel17082006@gmail.com>
2025-07-20 13:49:25 +0530

    completed

---
+++ Comment_Review(StudentForm)/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import StudentForm from './Components/StudentForm.jsx'
+
+function App() {
+  return (
+    <div>
+      <StudentForm />
+    </div>
+  )
+
+}
+
+export default App
