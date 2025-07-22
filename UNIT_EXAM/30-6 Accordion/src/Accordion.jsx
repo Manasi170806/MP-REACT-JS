a7cd8809977f1b3a022d3da33c86265ebc00ae1b
Manasi Patel <manasipatel17082006@gmail.com>
2025-07-22 12:29:38 +0530

    Update Accordion.jsx

--- UNIT_EXAM/30-6 Accordion/src/Accordion.jsx
+++ UNIT_EXAM/30-6 Accordion/src/Accordion.jsx
@@ -8,10 +8,14 @@ function Accordion(props) {
 
     return !(list.length > 0) ? <p>No items available</p> : (
         <div>
+            <h1>Accordion</h1>
+            <hr />
             {list.map((el,index)=>(
             <div className="accordion">
                 <div>
-                    <button onClick={()=>setIndex(index == startIndex ? null : index)} className="accordion-title">{el.title}</button>
+                    <button onClick={()=>setIndex(index == startIndex ? null : index)} className="accordion-title">{el.title}
+                        <span style={{ float: "right" }}>{index == startIndex ? <FaChevronUp /> : <FaChevronDown />}</span>
+                    </button>
                 </div>
                { index==startIndex && (<div className="accordion-content">
                     <p>{el.content}</p>
