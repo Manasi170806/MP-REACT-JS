f9aac0f72dd5fdec44c38186ac018a65c0bce45d
Manasi Patel <manasipatel17082006@gmail.com>
2025-07-26 14:21:28 +0530

    Update App.jsx

--- Integrate Bootstrap template/src/App.jsx
+++ Integrate Bootstrap template/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
 import './App.css'
+import Content from './Components/Content.jsx'
+import Buttons from './Components/Button.jsx'
+import Img from './Components/Img.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className="App">
-      </div>
-    </>
+    <div className="container container-lg w-50 mt-5">
+      <Content />
+      <Buttons />
+      <Img />
+    </div>
   )
 }
 
-export default App
+export default App;
